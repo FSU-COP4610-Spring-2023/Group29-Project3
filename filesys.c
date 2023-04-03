68fe3299e0ca51e70562ccae5f69e7d24cd67328
dpijeira1 <danielpijeira1@gmail.com>
2023-04-03 15:50:45 -0400

    Updated filesys.c with starter_file.c provided and added argument error checking

--- filesys.c
+++ filesys.c
@@ -1,121 +1,202 @@
+// starter file provided by operating systems ta's
+
+// include libraries
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
+#define BUFSIZE 40
+
+#define PATH_SIZE            4096   // max possible size for path string.
+#define OPEN_FILE_TABLE_SIZE 10     // max files for files. 
+#define MAX_NAME_LENGTH      11     // 11 in total but 3 for extensions, we only use 8.
+
+
+// data structures for FAT32 
+// Hint: BPB, DIR Entry, Open File Table -- how will you structure it?
+
+
+
+
+// stack implementaiton -- you will have to implement a dynamic stack
+// Hint: For removal of files/directories
+
+
 typedef struct {
-	int size;
-	char **items;
+    char path[PATH_SIZE];           // path string
+    // add a variable to help keep track of current working directory in file.
+    // Hint: In the image, where does the first directory entry start?
+} CWD;
+
+typedef struct  {
+    int size;
+    char ** items;
 } tokenlist;
 
-char *get_input(void);
-tokenlist *get_tokens(char *input);
+tokenlist * tokenize(char * input);
+void free_tokens(tokenlist * tokens);
+char * get_input();
+void add_token(tokenlist * tokens, char * item);
+void add_to_path(char * dir);
 
-tokenlist *new_tokenlist(void);
-void add_token(tokenlist *tokens, char *item);
-void free_tokens(tokenlist *tokens);
+// global variables
+CWD cwd;
 
-int main(int argc, char * argv[])
-{
-	if(argc == 2)
-    {
-        printf("%s", argv[0]);  //executable name
-        printf("%s", argv[1]);  //first argument
+int main(int argc, char * argv[]) {
+    // error checking for number of arguments.
+    if(argc != 2) {
+        printf("invalid number of arguments.\n");
+        return -1; // return error indicator if number of args is not 2.
     }
-    
-    while (1) {
-		printf("> ");
+    // read and open argv[1] in file pointer.
+    if((fp = fopen(argv[1], "r+")) == NULL) { // r+ stands for read,write,append.
+        printf("%s does not exist\n.", argv[1]);
+        return -1;
+    }
+    // obtain important information from bpb as well as initialize any important global variables
+    memset(cwd.path, 0, PATH_SIZE);
 
-		/* input contains the whole command
-		 * tokens contains substrings from input split by spaces
-		 */
+    // parser
+    char *input;
 
-		char *input = get_input();
-		printf("whole input: %s\n", input);
+    while(1) {
+        printf("%s/>", cwd.path);
+        input = get_input();
+        tokenlist * tokens = tokenize(input);    
+        printf("tokens size: %d\n", tokens->size);
+        for(int i = 0; i < tokens->size; i++) {
+            printf("token %d: %s\n", i, tokens->items[i]);
+        }
+        //add_to_path(tokens->items[0]);      // move this out to its correct place;
+        free(input);
+        free_tokens(tokens);
+    }
 
-		tokenlist *tokens = get_tokens(input);
-		for (int i = 0; i < tokens->size; i++) {
-			printf("token %d: (%s)\n", i, tokens->items[i]);
-		}
-
-		free(input);
-		free_tokens(tokens);
-	}
-
-	return 0;
+    return 0;
 }
 
-tokenlist *new_tokenlist(void)
-{
-	tokenlist *tokens = (tokenlist *) malloc(sizeof(tokenlist));
-	tokens->size = 0;
-	tokens->items = (char **) malloc(sizeof(char *));
-	tokens->items[0] = NULL; /* make NULL terminated */
-	return tokens;
-}
+// helper functions -- to navigate file image
 
-void add_token(tokenlist *tokens, char *item)
-{
-	int i = tokens->size;
 
-	tokens->items = (char **) realloc(tokens->items, (i + 2) * sizeof(char *));
-	tokens->items[i] = (char *) malloc(strlen(item) + 1);
-	tokens->items[i + 1] = NULL;
-	strcpy(tokens->items[i], item);
 
-	tokens->size += 1;
-}
+// commands -- all commands mentioned in part 2-6 (17 cmds)
 
-char *get_input(void)
-{
-	char *buffer = NULL;
-	int bufsize = 0;
 
-	char line[5];
-	while (fgets(line, 5, stdin) != NULL) {
-		int addby = 0;
-		char *newln = strchr(line, '\n');
-		if (newln != NULL)
-			addby = newln - line;
-		else
-			addby = 5 - 1;
 
-		buffer = (char *) realloc(buffer, bufsize + addby);
-		memcpy(&buffer[bufsize], line, addby);
-		bufsize += addby;
-
-		if (newln != NULL)
-			break;
-	}
-
-	buffer = (char *) realloc(buffer, bufsize + 1);
-	buffer[bufsize] = 0;
-
-	return buffer;
-}
-
-tokenlist *get_tokens(char *input)
-{
-	char *buf = (char *) malloc(strlen(input) + 1);
-	strcpy(buf, input);
-
-	tokenlist *tokens = new_tokenlist();
-
-	char *tok = strtok(buf, " ");
-	while (tok != NULL) {
-		add_token(tokens, tok);
-		tok = strtok(NULL, " ");
-	}
-
-	free(buf);
-	return tokens;
+// add directory string to cwd path -- helps keep track of where we are in image.
+void add_to_path(char * dir) {
+    if(dir == NULL) {
+        return;
+    }
+    else if(strcmp(dir, "..") == 0) {     
+        char *last = strrchr(cwd.path, '/');
+        if(last != NULL) {
+            *last = '\0';
+        }
+    } else if(strcmp(dir, ".") != 0) {
+        strcat(cwd.path, "/");
+        strcat(cwd.path, dir);
+    }
 }
 
 void free_tokens(tokenlist *tokens)
 {
-	for (int i = 0; i < tokens->size; i++)
-		free(tokens->items[i]);
-	free(tokens->items);
-	free(tokens);
+    for (int i = 0; i < tokens->size; i++)
+        free(tokens->items[i]);
+    free(tokens->items);
+    free(tokens);
 }
 
+// take care of delimiters {'\"', ' '}
+tokenlist * tokenize(char * input) {
+    int is_in_string = 0;
+    tokenlist * tokens = (tokenlist *) malloc(sizeof(tokenlist));
+    tokens->size = 0;
 
+    tokens->items = (char **) malloc(sizeof(char *));
+    char ** temp;
+    int resizes = 1;
+    char * token = input;
+
+    for(; *input != '\0'; input++) {
+        if(*input == '\"' && !is_in_string) {
+            is_in_string = 1;
+            token = input + 1;
+        } else if(*input == '\"' && is_in_string) {
+            *input = '\0';
+            add_token(tokens, token);
+            while(*(input + 1) == ' ') {
+                input++;
+            }
+            token = input + 1;
+            is_in_string = 0;                    
+        } else if(*input == ' ' && !is_in_string) {
+            *input = '\0';
+            while(*(input + 1) == ' ') {
+                input++;
+            }
+            add_token(tokens, token);
+            token = input + 1;
+        }
+    }
+    if(is_in_string) {
+        printf("error: string not properly enclosed.\n");
+        tokens->size = -1;
+        return tokens;
+    }
+
+    // add in last token before null character.
+    if(*token != '\0') {
+        add_token(tokens, token);
+    }
+
+    return tokens;
+}
+
+void add_token(tokenlist *tokens, char *item)
+{
+    int i = tokens->size;
+    tokens->items = (char **) realloc(tokens->items, (i + 2) * sizeof(char *));
+    tokens->items[i] = (char *) malloc(strlen(item) + 1);
+    tokens->items[i + 1] = NULL;
+    strcpy(tokens->items[i], item);
+    tokens->size += 1;
+}
+
+char * get_input() {
+    char * buf = (char *) malloc(sizeof(char) * BUFSIZE);
+    memset(buf, 0, BUFSIZE);
+    char c;
+    int len = 0;
+    int resizes = 1;
+
+    int is_leading_space = 1;
+
+    while((c = fgetc(stdin)) != '\n' && !feof(stdin)) {
+
+        // remove leading spaces.
+        if(c != ' ') {
+            is_leading_space = 0;
+        } else if(is_leading_space) {
+            continue;
+        }
+        
+        buf[len] = c;
+
+        if(++len >= (BUFSIZE * resizes)) {
+            buf = (char *) realloc(buf, (BUFSIZE * ++resizes) + 1);
+            memset(buf + (BUFSIZE * (resizes - 1)), 0, BUFSIZE);
+        }        
+    }
+    buf[len + 1] = '\0';
+
+    // remove trailing spaces.
+    char * end = &buf[len - 1];
+
+    while(*end == ' ') {
+        *end = '\0';
+        end--;
+    }   
+
+    return buf;
+}
